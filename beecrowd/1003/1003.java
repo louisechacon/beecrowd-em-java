80fcf52b4d8ca81525418138bcfd8034fdc275d6
Louise Chacon <85699734+louisechacon@users.noreply.github.com>
2025-08-03 19:32:14 -0300

    Create 1003.java

---
+++ beecrowd/1003/1003.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main {
+ 
+    public static void main(String[] args) {
+        
+        Scanner leitor = new Scanner(System.in);
+        
+        int A = leitor.nextInt();
+        int B = leitor.nextInt();
+        int SOMA = A + B;
+        
+        System.out.print("SOMA = ");
+        System.out.println(SOMA);
+ 
+    }
+ 
+}
