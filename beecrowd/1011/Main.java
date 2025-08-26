98782e28b6656032dd9c89f79b0935c5343532f3
Louise Chacon <85699734+louisechacon@users.noreply.github.com>
2025-08-26 18:07:54 -0300

    Create Main.java

---
+++ beecrowd/1011/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+ 
+    public static void main(String[] args) {
+ 
+       Scanner leitor = new Scanner(System.in);
+       double raio = leitor.nextDouble();
+       double pi = 3.14159;
+       double volume = (4/3.0) * pi * (raio * raio * raio);
+       
+       System.out.print("VOLUME = ");
+       System.out.printf("%.3f%n", volume);
+ 
+    }
+ 
+}
