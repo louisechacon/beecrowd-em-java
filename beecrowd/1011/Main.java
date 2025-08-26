276a67557990be89bbc8af01296c921d9e2f9c21
Louise Chacon <85699734+louisechacon@users.noreply.github.com>
2025-08-26 18:18:26 -0300

    Update Main.java

--- beecrowd/1011/Main.java
+++ beecrowd/1011/Main.java
@@ -7,7 +7,7 @@ public class Main {
        Scanner leitor = new Scanner(System.in);
        double raio = leitor.nextDouble();
        double pi = 3.14159;
-       double volume = (4/3.0) * pi * (raio * raio * raio);
+       double volume = (4/3.0) * pi * (Math.pow(raio,3));
        
        System.out.print("VOLUME = ");
        System.out.printf("%.3f%n", volume);
